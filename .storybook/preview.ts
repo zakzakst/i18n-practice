c1da84a0fccbd453b5752a1ad5da0736e1022974
zakzakst <e1433104@gmail.com>
2024-08-25 20:54:13 +0900

    修正

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,20 @@
 import type { Preview } from "@storybook/react";
+import axe from "axe-core";
 import AXE_LOCALE_JA from "axe-core/locales/ja.json";
 // import {
 //   INITIAL_VIEWPORTS,
 //   MINIMAL_VIEWPORTS,
 // } from "@storybook/addon-viewport";
+
+// const a11yNoRules = axe.getRules().map((rule) => ({
+//   id: rule.ruleId,
+//   enabled: false,
+// }));
+
+// const a11yWcag2aRules = axe.getRules(["wcag2a"]).map((rule) => ({
+//   id: rule.ruleId,
+//   enabled: true,
+// }));
 
 const customViewports = {
   kindleFire2: {
@@ -34,6 +45,14 @@ const preview: Preview = {
     a11y: {
       config: {
         locale: AXE_LOCALE_JA,
+        // rules: [
+        //   ...a11yNoRules,
+        //   ...a11yWcag2aRules,
+        //   {
+        //     id: "color-contrast",
+        //     enabled: false,
+        //   },
+        // ],
       },
     },
     viewport: {
