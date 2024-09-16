a41a7bb5d00e1e8b685cf2d12def1379bedfcd25
zakzakst <e1433104@gmail.com>
2024-09-16 21:28:40 +0900

    修正

--- tests/axe-core.spec.ts
+++ tests/axe-core.spec.ts
@@ -1,18 +1,39 @@
 import { test, expect } from "@playwright/test";
 import { AxeBuilder } from "@axe-core/playwright";
+// import { createHtmlReport } from "axe-html-reporter";
 
-test("accessibility test", async ({ page }) => {
+// TODO: 対象の複数ページに対して実行する
+
+test("accessibility test", async ({ page }, testInfo) => {
   await page.goto("https://s-ishizaki.sakura.ne.jp/sample/01/");
 
   // axe-core を使ってアクセシビリティテストを実行
   // const results = await new AxeBuilder({ page }).analyze();
-  const results = await new AxeBuilder({ page }).withTags(["wcag2a"]).analyze();
+  const results = await new AxeBuilder({ page })
+    .include("main")
+    // .exclude("iframe")
+    .withTags(["wcag2a"])
+    .analyze();
 
   // アクセシビリティテストの結果を出力
-  results.violations.forEach((violation) => {
-    // TODO: CSVに出力
-    console.log(violation);
-  });
+  // results.violations.forEach((violation) => {
+  //   console.log(violation);
+  // });
+
+  // アクセシビリティテストの結果をCSVに出力
+  if (results.violations.length > 0) {
+    console.log(results.violations[0]);
+    // const { id, description, nodes } = results.violations[0];
+    // console.log(id, description, testInfo.project.name);
+    // const { html, failureSummary } = nodes[0];
+    // console.log(html, failureSummary);
+    // TODO: CSVに出力（violationsとnodesで二回層のループ必要そう）
+  }
+
+  // レポート出力
+  // if (results.violations.length > 0) {
+  //   createHtmlReport({ results });
+  // }
 
   // アクセシビリティテストの結果がエラーがないことを確認
   expect(results.violations.length).toBe(0);
